77ea2a79c7dbc69a79ecae5fc2821ac822456732
zainab66 <zainab66r@yahoo.com>
2020-08-27 00:02:24 +0000

    Cats as a Service

---
+++ breedFetcher.js
@@ -0,0 +1,31 @@
+/*
+const request = require('request');
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+  //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+  //console.log(typeof body)
+
+  const data = JSON.parse(body);
+  //console.log(data);
+  //console.log(typeof data);
+  console.log(data[0].weight)
+  console.log(data[0].description)
+console.log(process. argv)
+});
+
+*/
+const request = require('request');
+
+request(('https://api.thecatapi.com/v1/breeds/search' + "?q=" + process.argv[2]), (error, response, body) => {
+  const data = JSON.parse(body);
+  if (process.argv.length === 2) {
+    console.log('please enter a bird name');
+  } else {
+    if (data[0] === undefined) {
+      console.log('The bird name is not found');
+    } else {
+      console.log(data[0].description);
+    }
+  }
+});
